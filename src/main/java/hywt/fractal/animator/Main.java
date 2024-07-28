01e644fb649e1de33c8157648df598cf3609dcfd
havey <haveyouwantto@gmail.com>
2024-07-28 21:37:16 +0800

    add default setting file detection

--- src/main/java/hywt/fractal/animator/Main.java
+++ src/main/java/hywt/fractal/animator/Main.java
@@ -2,11 +2,25 @@ package hywt.fractal.animator;
 
 import com.formdev.flatlaf.FlatDarculaLaf;
 import hywt.fractal.animator.ui.GUI;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String[] args) {
         FlatDarculaLaf.setup();
         GUI gui = new GUI();
+
+        File defaults = new File("default.fap");
+        if (defaults.exists()) {
+            try {
+                DataInputStream is = new DataInputStream(new FileInputStream(defaults));
+                String content = new String(is.readAllBytes(), StandardCharsets.UTF_8);
+                gui.importJSON(new JSONObject(content));
+            } catch (IOException ignored) {}
+        }
+
         gui.setVisible(true);
     }
 }
